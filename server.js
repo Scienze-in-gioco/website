643d91e602b18ff1b4ca6b543541f2feae0f2067
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-12-18 10:43:26 +0100

    iniziato lavoro query

--- server.js
+++ server.js
@@ -1,30 +1,80 @@
 const express = require("express")
 const app = new express()
-const { p, port = p || 8080, env = process.env.NODE_ENV || "local" } = require("simple-argv")
+const { 
+  p,
+  port = p || 8080,
+  env = process.env.NODE_ENV || "local" 
+} = require("simple-argv")
+const TABLES = ["contents", "tags", "attachments", "categories"]
 const mysql = require("mysql")
 
 const config = require(`./db-config-${env}`)
 const pool = mysql.createPool({
   connectionLimit : 10,
-  multipleStatements: true,
   ...config
+})
+
+app.use((req, res, next) => {
+  res.error = (err, status = 500) => {
+    res.status(status).json({
+      error: env == "production" ? err.message : err.stack
+    })
+  }
+
+  next()
 })
 
 app.get("/", (req, res) => {
   res.send("questa sarà la nostra home page")
 })
 
-app.get("/api/contents", (req, res) => {
-  pool.query("SELECT * FROM contents", (err, results) => {
+TABLES.forEach(table => {
+
+  app.get(`/api/${table}`, (req, res) => {
+    pool.query(`SELECT * FROM ${table}`, (err, results) => {
+      if (err) {
+        res.error(err)
+      } else {
+        res.json({
+          results
+        })
+      }
+    })
+  })
+
+})
+
+app.get("/api/c", (req, res) => {
+  pool.query(`
+SELECT 
+  c.permalink, 
+  c.title,
+  c.body,
+  c.author,
+  c.draft,
+  c.featured,
+  c.pubblicationDate,
+  t.name AS tagName,
+  cat.name AS categoryName
+
+FROM Contents AS c
+
+INNER JOIN ContentsAttachmentsTh ON Contents.id = ContentsAttachmentsTh.contentId
+INNER JOIN Attachments AS a ON ContentsAttachmentsTh.attachmentId = Attachments.id
+
+INNER JOIN ContentsCategoriesTh ON Contents.id = ContentsCategoriesTh.contentId
+INNER JOIN Categories AS cat ON ContentsCategoriesTh.categoryId = Categories.id
+
+INNER JOIN ContentsTagsTh ON Contents.id = ContentsTagsTh.contentId
+INNER JOIN Tags AS t ON ContentsTagsTh.tagId = Tags.id
+    `, (err, results) => {
     if (err) {
-      res.status(500).json({
-        error: env == "production" ? err.message : err.stack
-      })
-    } else {
-      res.json({
-        results
-      })
-    }
+        res.error(err)
+      } else {
+        res.json({
+          results
+        })
+      }
   })
 })
 
